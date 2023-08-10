c8cd8b7ab3e062fca06822d05bcd28a9a6f1a024
Faruk Spahic <farukspahictz@gmail.com>
2023-08-10 15:04:26 +0200

    Use vite instead of webpack

--- frontend/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="text-2xl font-bold"></div>;
-}
-
-export default App;
--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,10 +1,11 @@
+import "./index.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
