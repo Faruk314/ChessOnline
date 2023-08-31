6f4264d4118f3f7e1c67143dd5e066387f758fb6
Faruk Spahic <farukspahictz@gmail.com>
2023-08-31 13:17:50 +0200

    Added custom error handler and auth routes

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import authRoutes from "./routes/auth";
+import errorHandler from "./utils/error";
 
 const app = express();
-
-app.get("/hello", (_, res) => {
-  res.send("Hello V !");
-});
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
+
+app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
