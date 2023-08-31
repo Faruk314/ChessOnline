d04907193c54100fb487a7cfe0e46e7bbf58ac37
Faruk Spahic <farukspahictz@gmail.com>
2023-08-31 16:20:05 +0200

    Added getLoginStatus func to app.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,15 +1,36 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Board from "./components/Board";
 import Menu from "./pages/Menu";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import axios from "axios";
 import Register from "./pages/Register";
+import { AuthContext } from "./context/AuthContext";
 
 axios.defaults.withCredentials = true;
 // axios.defaults.baseURL = process.env.FRONTEND_URL;
 
 function App() {
+  const { setIsLoggedIn, setLoggedUserInfo } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getLoginStatus = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/api/auth/getLoginStatus"
+        );
+
+        setIsLoggedIn(response.data.status);
+        setLoggedUserInfo(response.data.userInfo);
+      } catch (error) {
+        console.log(error);
+        setIsLoggedIn(false);
+      }
+    };
+
+    getLoginStatus();
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
