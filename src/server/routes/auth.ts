a1123ebbce5c833cb355c37ccc3e02b0a78ec818
Faruk Spahic <farukspahictz@gmail.com>
2023-08-31 14:16:10 +0200

    Added logout controller

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import { login, register } from "../controllers/auth";
+import { login, logout, register } from "../controllers/auth";
 
 const router = express.Router();
 
 router.get("/register", register);
 
 router.get("/login", login);
+
+router.get("/logout", logout);
 
 export default router;
